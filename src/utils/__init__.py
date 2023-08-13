72e557f0c9eece6a196a3208edb6e94efd8dcd2a
win 123 <123>
2023-08-14 00:40:34 +0800

    add pkl

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,5 @@ from src.utils.logging_utils import log_hyperparameters
 from src.utils.pylogger import get_pylogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
 from src.utils.utils import extras, get_metric_value, task_wrapper
+from .visualization import (show_video_line, show_video_gif_multiple, show_video_gif_single,
+                            show_heatmap_on_image, show_taxibj, show_weather_bench)
